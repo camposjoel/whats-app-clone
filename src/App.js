2223cdb6d4c0623b025bf94ae2ab4c35786557af
Joel Campos <underkillr@gmail.com>
2020-09-18 22:13:36 -0500

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Chat from './components/Chat';
+import Login from './components/Login';
+import Sidebar from './components/Sidebar';
+import { useStateValue } from './StateProvider';
 
 function App() {
+
+  const [{ user }, dispatch] = useStateValue(); 
+  const [userSession, setUserSession] = useState(() => {
+    return window.sessionStorage.getItem('user');
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!user && !userSession ? (
+        <Login />
+      ) : (
+          <div className="app__body">
+            <Router>
+              <Sidebar />
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <Chat />
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+        )
+      }
+
+      <p>Joel Campos 🇲🇽</p>
     </div>
   );
 }
